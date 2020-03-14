de20ad1aa6e731e78bc50122e6e0945a87cb4878
marthaserpak <m.v.serpak@gmail.com>
2020-03-14 13:52:31 +0200

    codelab

--- app/src/main/java/com/example/roomwordssample/Word.java
+++ app/src/main/java/com/example/roomwordssample/Word.java
@@ -1,9 +1,17 @@
 package com.example.roomwordssample;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+//Annotate the Word class to be meaningful to a Room database.
+@Entity (tableName = "word_table")
 public class Word {
 
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
     private String mWord;
 
     public Word(@NonNull String word) {
