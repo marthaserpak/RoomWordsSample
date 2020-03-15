fdb5f932ebec821912a3ba9dd7bea6ac4a2afa9d
marthaserpak <m.v.serpak@gmail.com>
2020-03-15 17:24:43 +0200

    codelab

---
+++ app/src/main/java/com/example/roomwordssample/WordViewModel.java
@@ -0,0 +1,22 @@
+package com.example.roomwordssample;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+    private LiveData<List<Word>> mAllWords;
+
+    
+    public WordViewModel(Application application){
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+}
