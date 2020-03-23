2241989c13dc3b8e6aa26df5812dc7963555eb69
marthaserpak <m.v.serpak@gmail.com>
2020-03-23 12:47:27 +0200

    codelab

--- app/src/main/java/com/example/roomwordssample/WordRoomDataBase.java
+++ app/src/main/java/com/example/roomwordssample/WordRoomDataBase.java
@@ -1,10 +1,13 @@
 package com.example.roomwordssample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDataBase extends RoomDatabase {
@@ -25,10 +28,47 @@ public abstract class WordRoomDataBase extends RoomDatabase {
                             //if no Migration object.
                             //Migration is not part of this practical
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    //Add the onOpen() callback in the WordRoomDatabase class
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    private static class PpopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final  WordDao mDao;
+        String[] words = {"dolphin", "crocodile", "cobra", "monkey",
+        "elephant", "bird\"Dodo\"", "mouse"};
+
+        PpopulateDbAsync(WordRoomDataBase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            //Start the app with clean database every time.
+            //Not needed if you only populate the database
+            //when it first created
+            mDao.deleteAllWords();
+
+            for (int i = 0; i< words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+
+            return null;
+        }
     }
 }
